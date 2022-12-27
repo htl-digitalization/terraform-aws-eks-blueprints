56be268a56abf47801e7bddbc9490f66516bc23a
hieutrluu <thl1g15@outlook.com>
2022-12-27 19:26:11 +0700

    Revert "update vpc-cni not using nat gatway"
    This reverts commit e79441a0a3939b5ace8886f3ebe2ac0bc566d282.

--- examples/vpc-cni-custom-networking/main.tf
+++ examples/vpc-cni-custom-networking/main.tf
@@ -238,9 +238,9 @@ module "vpc" {
   public_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
   intra_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
 
-  enable_nat_gateway   = false
-  single_nat_gateway   = false
-  enable_dns_hostnames = false
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
 
   # Manage so we can name
   manage_default_network_acl    = true
